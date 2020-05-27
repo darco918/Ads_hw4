45dda441ab3a63c6fae989a2b9007b35f86353d3
Darco Murja <darcomurja@gmail.com>
2020-05-27 16:33:39 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/ADS4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ADS4.exe"
+  "ADS4.exe.manifest"
+  "ADS4.lib"
+  "ADS4.pdb"
+  "CMakeFiles/ADS4.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ADS4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
